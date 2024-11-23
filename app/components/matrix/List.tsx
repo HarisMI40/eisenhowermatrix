3817d05d18e5aedf312fc0ff6605e8b0a0ea87a1
HarisMI40 <harismaulanaikhsan40@gmail.com>
2024-11-24 01:48:31 +0700

    add : add feature remaining day in list

--- app/components/matrix/List.tsx
+++ app/components/matrix/List.tsx
@@ -3,11 +3,32 @@ import { useDispatch } from 'react-redux'
 import { setSelectedTask } from '@/lib/store/tasksSlice'
 import { Card } from "@/components/ui/card"
 import TaskMenu from "./TaskMenu"
-import { format } from "date-fns"
+import { differenceInDays, format } from "date-fns"
 import { Clock } from "lucide-react"
 
 const List = ({ filteredTasks }: { filteredTasks: Task[] }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  const showRemainingDay = (startDate: string) => {
+    const remainingDay = differenceInDays(startDate, new Date());
+    let bgColor = "";
+
+    if (remainingDay > 4) {
+      bgColor = "bg-green-500";
+    }
+    else if (remainingDay > 0 && remainingDay < 4 ) {
+      bgColor = "bg-orange-500";
+    }
+    else {
+      bgColor = "bg-red-500";
+    }
+
+    return (
+      <span className={`text-xs px-1 ml-2 font-semibold ${bgColor} rounded`}>
+        {`${remainingDay} days remaining`}
+      </span>
+    );
+  };
 
   return (
     <ul className="list-none space-y-2">
@@ -23,8 +44,15 @@ const List = ({ filteredTasks }: { filteredTasks: Task[] }) => {
                 <div className="flex items-center space-x-2">
                   <p className="text-sm">{task.text}</p>
                 </div>
-                { task.startDate && (
-                  <div className="text-xs text-black px-1 rounded bg-gray-200 w-max flex gap-1 items-center"> <Clock size={12} /> {format(task.startDate, "dd MMM yyyy")}</div>
+                {task.startDate && (
+                  <div className="flex gap-3">
+
+                    <div className="text-xs text-black px-1 rounded bg-gray-200 w-max flex gap-1 items-center">
+                      <Clock size={12} /> {format(task.startDate, "dd MMM yyyy")}
+                    </div>
+
+                    {showRemainingDay(task.startDate)}
+                  </div>
                 )}
               </div>
               <TaskMenu task={task} />
